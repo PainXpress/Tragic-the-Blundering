cc0b33fea9865f046161515d9011505f91251a22
Jason Perry <jperry75007@gmail.com>
2023-04-16 14:25:48 -0400

    I am commited

--- modules/game.py
+++ modules/game.py
@@ -14,7 +14,7 @@ screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Tragic The Blundering")
 
 # Load the background image
-background_image = pygame.image.load("menu background.png").convert()
+background_image = pygame.image.load("assets/images/menu_background.png").convert()
 
 # Resize the background image to fit the window
 background_image = pygame.transform.scale(background_image, (1920, 1080))
